df5759ed232c8cf93b77adf5766bd3d2c4b567ef
Akash Kumar <akashbes@gmail.com>
2024-03-05 23:45:39 +0530

    After video 11

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -20,6 +20,16 @@ export default function TextForm(props) {
         let newText = '';
         setText(newText)
     }
+
+    const handleCopyClick = ()=>{
+        var text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+    const handleExtraSpaces = ()=>{
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+    }
     const [text, setText] = useState('Enter text here');
     // text = "new text" //wrong way to change the state
     // setText("new text") //Correct way to change the state
@@ -33,6 +43,8 @@ export default function TextForm(props) {
         <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
         <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to LowerCase</button>
         <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear text</button>
+        <button className="btn btn-primary mx-2" onClick={handleCopyClick}>Copy text</button>
+        <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
 
     </div>
     <div className="container my-3">
